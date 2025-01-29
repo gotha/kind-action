866a1f436b7fecc9e11831408fa3e51e65e6d4a8
Hristo Georgiev <h.georgiev@hotmail.com>
2025-01-29 10:34:02 +0200

    dbg 4

--- test1.sh
+++ test1.sh
@@ -9,17 +9,20 @@ install_kubectl() {
     chmod +x "${kubectl_dir}/kubectl"
 }
 
-local cache_dir="${RUNNER_TOOL_CACHE}/kind/${version}/${arch}"
-echo "cache_dir: $cache_dir"
+main() {
+  local cache_dir="${RUNNER_TOOL_CACHE}/kind/${version}/${arch}"
+  echo "cache_dir: $cache_dir"
+  
+  local kubectl_dir="${cache_dir}/kubectl/bin/"
+  echo "kubectl_dir: $kubectl_dir"
+  if [[ ! -x "${kubectl_dir}/kubectl" ]]; then
+      install_kubectl
+  fi
+  
+  echo 'Adding kubectl directory to PATH...'
+  echo "${kubectl_dir}" >> "${GITHUB_PATH}"
+}
 
-local kubectl_dir="${cache_dir}/kubectl/bin/"
-echo "kubectl_dir: $kubectl_dir"
-if [[ ! -x "${kubectl_dir}/kubectl" ]]; then
-    install_kubectl
-fi
-
-echo 'Adding kubectl directory to PATH...'
-echo "${kubectl_dir}" >> "${GITHUB_PATH}"
-
+main
 echo "test1 done"
 echo "======================"
