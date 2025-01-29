68d6d4a95f75e8a17b5201429ad398fa3305c34f
Hristo Georgiev <h.georgiev@hotmail.com>
2025-01-29 13:35:44 +0200

    dbg 1.6

--- test1.sh
+++ test1.sh
@@ -1,36 +1,30 @@
 #!/usr/bin/env bash
 
-main() {
-  echo $(uname -m)
-  local version="v1.31.4"
-  local arch
-  case $(uname -m) in
-      i686)               arch="386" ;;
-      x86_64)             arch="amd64" ;;
-      arm|aarch64|arm64)  arch="arm64" ;;
-      *) exit 1 ;;
-  esac
+version="v1.31.4"
+arch
+case $(uname -m) in
+    i686)               arch="386" ;;
+    x86_64)             arch="amd64" ;;
+    arm|aarch64|arm64)  arch="arm64" ;;
+    *) exit 1 ;;
+esac
 
-  local cache_dir="${RUNNER_TOOL_CACHE}/tmp/${version}/${arch}"
-  echo "cache_dir: $cache_dir"
+cache_dir="${RUNNER_TOOL_CACHE}/tmp/${version}/${arch}"
+echo "cache_dir: $cache_dir"
 
-  local kubectl_dir="${cache_dir}/kubectl/bin/"
-  echo "kubectl_dir: $kubectl_dir"
-  mkdir -p "${kubectl_dir}"
-  ls -las "$kubectl_dir"
+kubectl_dir="${cache_dir}/kubectl/bin/"
+echo "kubectl_dir: $kubectl_dir"
+mkdir -p "${kubectl_dir}"
+ls -las "$kubectl_dir"
 
-  echo "https://dl.k8s.io/release/${version}/bin/linux/${arch}/kubectl"
-  curl -sSLo "${kubectl_dir}/kubectl" "https://dl.k8s.io/release/${version}/bin/linux/${arch}/kubectl"
-  chmod +x "${kubectl_dir}/kubectl"
+echo "https://dl.k8s.io/release/${version}/bin/linux/${arch}/kubectl"
+curl -sSLo "${kubectl_dir}/kubectl" "https://dl.k8s.io/release/${version}/bin/linux/${arch}/kubectl"
+chmod +x "${kubectl_dir}/kubectl"
 
-  echo 'Adding kubectl directory to PATH...'
-  echo "${kubectl_dir}" >> "${GITHUB_PATH}"
-  echo "........................"
-  cat "$GITHUB_PATH"
-  echo "x........................"
-  which kubectl
-  echo "........................"
-}
-
-main
-
+echo 'Adding kubectl directory to PATH...'
+echo "${kubectl_dir}" >> "${GITHUB_PATH}"
+echo "........................"
+cat "$GITHUB_PATH"
+echo "x........................"
+which kubectl
+echo "........................"
