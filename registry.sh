e4f59c14a38cc6ff51ae2d67c38a0c8f832ecf98
Hristo Georgiev <h.georgiev@hotmail.com>
2025-01-30 23:16:38 +0200

    dbg

--- registry.sh
+++ registry.sh
@@ -140,12 +140,17 @@ connect_registry() {
 }
 
 config_registry_for_nodes() {
-    if [ -f "$GITHUB_PATH" ]; then
-        while IFS= read -r line; do
-            echo "ghpath: $line"
-            export PATH="$PATH:$line"
-        done < "$GITHUB_PATH"
-    fi
+
+    echo "which kind ======================="
+    which kind
+    echo "which kind end===================="
+    #if [ -f "$GITHUB_PATH" ]; then
+    #    while IFS= read -r line; do
+    #        echo "ghpath: $line"
+    #        export PATH="$PATH:$line"
+    #    done < "$GITHUB_PATH"
+    #fi
+
     # Reference: https://github.com/containerd/containerd/blob/main/docs/hosts.md
     REGISTRY_DIR="/etc/containerd/certs.d/${registry_name}:${registry_port}"
 
