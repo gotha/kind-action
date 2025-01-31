051b7c72a731e4907635b2bf3e726981c3368553
Hristo Georgiev <h.georgiev@hotmail.com>
2025-01-31 07:23:19 +0200

    dbg

--- registry.sh
+++ registry.sh
@@ -183,5 +183,13 @@ data:
 EOF
 }
 
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo "registry.sh is starting"
+echo "which kubectl ............................"
+which -a kubectl || echo "kubectl is missing"
+echo "which kubectl end ........................"
+echo "which kind .............................."
+which -a kind || echo "kind is missing"
+echo "which kind end ......................."
+echo "======================================="
 main "$@"
